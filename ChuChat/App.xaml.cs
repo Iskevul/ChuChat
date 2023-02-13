bbdeaf15304db1c8cbb19b4336e7a1f132853ec7
Искандер Шарафеев <iskin20003@gmail.com>
2023-02-13 19:02:14 +0300

    Добавил авторизацию

--- ChuChat/App.xaml.cs
+++ ChuChat/App.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ChuChat.Model;
 using System.Windows;
 
 namespace ChuChat
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public static Employee Employee { get; set; }
     }
 }
---
+++ ChuChat/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ChuChat.DB;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ChuChat.Pages
+{
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+            tbName.Text = Properties.Settings.Default.Username;
+        }
+
+        private void OkBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var username = tbName.Text;
+            var password = pbPassword.Password.ToString();
+            if (DataAccess.GetEmployee(username, password) != null)
+            {
+                App.Employee = DataAccess.GetEmployee(username, password);
+
+                if ((bool)cbRememberMe.IsChecked)
+                {
+                    Properties.Settings.Default.Username = App.Employee.Username;
+                    Properties.Settings.Default.Save();
+                }
+                //NavigationService.Navigate(new Pages.ChatListPage());
+            }
+            else
+                MessageBox.Show("Unknown username or password");
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.MainWindow.Close();
+        }
+    }
+}
